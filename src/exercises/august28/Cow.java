26236270b65adfb58225ed2add0151861920f6c6
Gareth Coles <gdude2002@gmail.com>
2014-08-28 14:04:34 +0100

    Moo!

--- src/exercises/august28/Cow.java
+++ src/exercises/august28/Cow.java
@@ -6,11 +6,15 @@ package exercises.august28;
 public class Cow {
 
     public static void main(String[] args) {
+        System.out.println("");
         System.out.println("          '__'");
         System.out.println("          (oo)");
         System.out.println("  +========\\/");
         System.out.println(" / || %%% ||");
         System.out.println("*  ||-----||");
         System.out.println("   \"\"     \"\"");
+        System.out.println("");
+        System.out.println("Moo!");
+        System.out.println("");
     }
 }
