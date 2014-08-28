1dd9c17e6bec40f3626063d20976ed24debad424
Gareth Coles <gdude2002@gmail.com>
2014-08-28 14:03:42 +0100

    Cow. That wasn't so hard!

---
+++ src/exercises/august28/Cow.java
@@ -0,0 +1,16 @@
+package exercises.august28;
+
+/**
+ * Created by bw5 on 28/08/2014.
+ */
+public class Cow {
+
+    public static void main(String[] args) {
+        System.out.println("          '__'");
+        System.out.println("          (oo)");
+        System.out.println("  +========\\/");
+        System.out.println(" / || %%% ||");
+        System.out.println("*  ||-----||");
+        System.out.println("   \"\"     \"\"");
+    }
+}
